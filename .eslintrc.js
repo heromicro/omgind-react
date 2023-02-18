3e5df09f3e20468908422abe5a66e0202b5d30dc
hello <keye.myria@gmail.com>
2023-02-18 18:10:24 +0800

    seems ok

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'prettier', 'plugin:compat/recommended'],
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'eslint:recommended',
+    'plugin:compat/recommended',
+    'plugin:react/jsx-runtime',
+  ],
   env: {
     browser: true,
     node: true,
@@ -19,6 +31,10 @@ module.exports = {
     'react/prop-types': 0,
     'react/forbid-prop-types': 0,
     'react/jsx-one-expression-per-line': 0,
+    'react/jsx-props-no-spreading': 'warn',
+    'react/state-in-constructor': 'warn',
+    'react/no-unused-class-component-methods': 'warn',
+    'react/sort-comp': 'warn',
     'import/no-unresolved': [2, { ignore: ['^@/', '^umi/'] }],
     'import/no-extraneous-dependencies': [
       2,
@@ -33,7 +49,7 @@ module.exports = {
     'jsx-a11y/anchor-is-valid': 0,
     'linebreak-style': 0,
     'import/extensions': 0,
-    'import/no-extraneous-dependencies': 0,
+    // 'import/no-extraneous-dependencies': 0,
     'no-unused-vars': 0,
     'prefer-const': 0,
     'no-irregular-whitespace': 0,
