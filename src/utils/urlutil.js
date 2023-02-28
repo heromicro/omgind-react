4723b8efe20cd918a34b322622e0e32680dbd350
hello <keye.myria@gmail.com>
2023-02-28 15:05:09 +0800

    make up sort key

--- src/utils/urlutil.js
+++ src/utils/urlutil.js
@@ -7,15 +7,6 @@ export const makeupSortKey = (sort) => {
   // Name_Order      string `form:"name__order"`       // asc desc
   // Sort_Order      string `form:"sort__order"`       // asc desc
   let nsort = {};
-  // for (let key in sort) {
-  //     nsort[`${key}__order`] = sort[key]
-  // }
-  // console.log(" -------- -- nsorts ", nsorts);
-
-  // eslint-disable-next-line
-  // for (const [key, val] of Object.entries(sort)) {
-  // nsort[`${key}__order`] = val;
-  // }
   Object.keys(sort).forEach((key) => {
     nsort[`${key}__order`] = sort[key];
   });
