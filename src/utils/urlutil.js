9f1195d9dab5a8881db62c8a8b3f6823d98cdb74
hello <keye.myria@gmail.com>
2023-02-28 15:04:44 +0800

    make up sort key

--- src/utils/urlutil.js
+++ src/utils/urlutil.js
@@ -1,20 +1,23 @@
 // import * as _ from 'lodash';
 
-
 export const makeupSortKey = (sort) => {
-    // {sort: "ascend", }
-    // {sort: "descend" }
-    // created_at__order 
-	// Name_Order      string `form:"name__order"`       // asc desc
-	// Sort_Order      string `form:"sort__order"`       // asc desc 
-    let nsort = {}
-    // for (let key in sort) {
-    //     nsort[`${key}__order`] = sort[key]
-    // }
-    // console.log(" -------- -- nsorts ", nsorts);
-    for (const [key, val] of Object.entries(sort)) {
-        // eslint-disable-next-line
-        nsort[`${key}__order`] = val;
-    }
-    return nsort
-}
+  // {sort: "ascend", }
+  // {sort: "descend" }
+  // created_at__order
+  // Name_Order      string `form:"name__order"`       // asc desc
+  // Sort_Order      string `form:"sort__order"`       // asc desc
+  let nsort = {};
+  // for (let key in sort) {
+  //     nsort[`${key}__order`] = sort[key]
+  // }
+  // console.log(" -------- -- nsorts ", nsorts);
+
+  // eslint-disable-next-line
+  // for (const [key, val] of Object.entries(sort)) {
+  // nsort[`${key}__order`] = val;
+  // }
+  Object.keys(sort).forEach((key) => {
+    nsort[`${key}__order`] = sort[key];
+  });
+  return nsort;
+};
