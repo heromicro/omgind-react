9ad7d6dbc2b9fdb42e7893cb07d65f3d8f7ac2e9
hello <keye.myria@gmail.com>
2023-03-06 13:51:53 +0800

    less import

--- src/pages/system/District/formMeta.ts
+++ src/pages/system/District/formMeta.ts
@@ -2,9 +2,9 @@
 // The Great Britain of United Kingdom
 export const DistrictFormSchema = {
   type: 'object',
-  column: 2,
   labelWidth: 120,
   displayType: 'column',
+  column: 2,
   properties: {
     input_a6S1dv: {
       title: '名称',
@@ -14,7 +14,6 @@ export const DistrictFormSchema = {
       },
       required: true,
       maxLength: 128,
-      description: '',
       bind: 'name',
       placeholder: '请输入名称',
     },
@@ -24,30 +23,28 @@ export const DistrictFormSchema = {
       props: {
         allowClear: true,
       },
-      maxLength: 128,
-      description: '',
       bind: 'name_en',
       placeholder: '请输入英语名称',
     },
     input_QHFPpT: {
       title: '短名称',
       type: 'string',
-      bind: 'sname',
       props: {
         allowClear: true,
       },
-      placeholder: '请输入短名称',
       maxLength: 64,
+      bind: 'sname',
+      placeholder: '请输入短名称',
     },
     input_ZGVbYu: {
       title: '英语短名称',
       type: 'string',
-      bind: 'sname_en',
       props: {
         allowClear: true,
       },
-      placeholder: '请输入英语短名称',
       maxLength: 64,
+      bind: 'sname_en',
+      placeholder: '请输入英语短名称',
     },
     select_Ntj6Yx: {
       title: '上级',
@@ -55,54 +52,54 @@ export const DistrictFormSchema = {
       enum: ['a', 'b', 'c'],
       enumNames: ['早', '中', '晚'],
       widget: 'select',
-      bind: 'pid',
       placeholder: '请选择上级',
+      bind: 'pid',
     },
     input_mYAR0H: {
       title: '简称',
       type: 'string',
-      placeholder: '请输入简称',
-      bind: 'abbr',
       props: {
         allowClear: true,
       },
       maxLength: 64,
+      placeholder: '请输入简称',
+      bind: 'abbr',
     },
     input_tOMu8C: {
       title: '行政名称',
       type: 'string',
-      bind: 'merge_name',
       props: {
         allowClear: true,
       },
-      maxLength: 256,
+      placeholder: '请输入行政名称',
+      bind: 'merge_name',
     },
     input_weWqJv: {
       title: '行政短名称',
       type: 'string',
-      bind: 'merge_sname',
       props: {
         allowClear: true,
       },
-      maxLength: 256,
+      placeholder: '请输入行政短名称',
+      bind: 'merge_sname',
     },
-    input__1118: {
+    input_Uh68U3: {
       title: '行政后缀',
       type: 'string',
       props: {
         allowClear: true,
       },
-      maxLength: 16,
+      placeholder: '请输入行政后缀',
       bind: 'suffix',
     },
     input_sICl7S: {
       title: '拼音',
       type: 'string',
-      bind: 'pinyin',
       props: {
         allowClear: true,
       },
-      maxLength: 128,
+      placeholder: '请输入拼音',
+      bind: 'pinyin',
     },
     input_H79d6K: {
       title: '简拼',
@@ -110,16 +107,14 @@ export const DistrictFormSchema = {
       props: {
         allowClear: true,
       },
-      maxLength: 128,
+      placeholder: '请输入简拼',
       bind: 'initials',
     },
     input_hUoeQR: {
       title: '区号',
       type: 'string',
-      props: {
-        allowClear: true,
-      },
-      maxLength: 8,
+      props: {},
+      placeholder: '请输入区号',
       bind: 'area_code',
     },
     input_wA6wgv: {
@@ -128,8 +123,7 @@ export const DistrictFormSchema = {
       props: {
         allowClear: true,
       },
-      maxLength: 8,
-      bind: 'zip_code',
+      placeholder: '请输入邮码',
     },
     switch_1DYOgA: {
       title: '是否有效',
@@ -138,30 +132,29 @@ export const DistrictFormSchema = {
       default: true,
       bind: 'is_active',
     },
-    switch_PtXQte: {
+    'switch_nFni-w': {
       title: '是否主要',
       type: 'boolean',
       widget: 'switch',
       bind: 'is_main',
     },
-    switch_bzDGvw: {
+    switch_1HLMM8: {
       title: '是否真实',
       type: 'boolean',
       widget: 'switch',
       default: true,
       bind: 'is_real',
     },
-    switch_GcTbuJ: {
+    switch_wTE0rL: {
       title: '是否热点',
       type: 'boolean',
       widget: 'switch',
       bind: 'is_hot',
     },
-    switch_orG0Zt: {
+    switch_zo7CJT: {
       title: '是否直辖',
       type: 'boolean',
       widget: 'switch',
-      default: false,
       bind: 'is_direct',
     },
   },
