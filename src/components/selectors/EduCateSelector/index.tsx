7fdfb6a6734c9d966ba3b656a201572376d3a645
hello <keye.myria@gmail.com>
2023-03-29 15:55:12 +0800

    add selector

---
+++ src/components/selectors/EduCateSelector/index.tsx
@@ -0,0 +1,110 @@
+import React, { PureComponent } from 'react';
+import { Select, SelectProps } from 'antd';
+import * as _ from 'lodash';
+import * as dictService from '@/services/sysdict';
+
+interface EduCateSelectorProps extends SelectProps {
+  dictId?: string;
+  debounceTimeOut?: number;
+  // onChange?: (value, option) => void;
+}
+
+interface EduCateSelectorState {
+  value: string;
+  options: [];
+  dictId?: string;
+}
+const defaultProps: EduCateSelectorProps = { debounceTimeOut: 400, dictId: '-' };
+
+class EduCateSelector extends PureComponent<EduCateSelectorProps, EduCateSelectorState> {
+  //
+
+  constructor(props: EduCateSelectorProps) {
+    super(props);
+    this.state = {
+      value: props.value,
+      options: [],
+      dictId: props.dictId,
+    };
+
+    this.fetchDictItems = _.debounce(this.fetchDictItems.bind(this), props.debounceTimeOut);
+    this.triggerChange = _.debounce(this.triggerChange.bind(this), 300);
+  }
+
+  static getDerivedStateFromProps(nextProps, state) {
+    if ('value' in nextProps) {
+      return {
+        ...state,
+        value: nextProps.value,
+      };
+    }
+    return state;
+  }
+
+  componentDidMount(): void {
+    const { dictId } = this.props;
+
+    this.fetchDictItems(dictId ? dictId : '-');
+  }
+
+  fetchDictItems = (did: string) => {
+    dictService.items(did, { name_en: 'gender' }).then((res) => {
+      const { code, burden } = res;
+      if (code === 0 && burden.length > 0) {
+        let gender = burden[0];
+        console.log(' ---- --- --- ==== gender = ', gender);
+        if (gender.items.length > 0) {
+          let nd = [];
+          gender.items.map((item) => {
+            return nd.push({
+              ...item,
+            });
+          });
+          this.setState({
+            options: nd,
+          });
+        }
+      } else {
+      }
+    });
+  };
+
+  onChange = (value: string, option) => {
+    this.triggerChange(value, option);
+  };
+
+  onSelect = (value: string, option) => {
+    this.triggerChange(value, option);
+  };
+
+  triggerChange = (value, option) => {
+    const { onChange } = this.props;
+    if (onChange) {
+      // console.log(' ------- ===== ----- ', option);
+      onChange(value, option);
+    }
+  };
+
+  render() {
+    const { ...restProps } = this.props;
+    const { value, options } = this.state;
+
+    return (
+      <Select
+        {...restProps}
+        onChange={(val, option) => {
+          this.onChange(val, option);
+        }}
+        onSelect={(val, option) => {
+          this.onSelect(val, option);
+        }}
+        options={options}
+        value={value}
+      />
+    );
+  }
+}
+
+EduCateSelector.defaultProps = defaultProps;
+
+export default EduCateSelector;
---
+++ src/components/selectors/PositionSelector/index.tsx
@@ -0,0 +1,114 @@
+import React, { PureComponent } from 'react';
+import { Select, SelectProps } from 'antd';
+import * as _ from 'lodash';
+
+import * as orgService from '@/services/orgorgan';
+
+interface PositionSelectorProps extends SelectProps {
+  debounceTimeOut?: number;
+  // onChange?: (value, option) => void;
+}
+
+interface PositionSelectorState {
+  value: string;
+  options: [];
+}
+
+const defaultProps: PositionSelectorProps = { debounceTimeOut: 400 };
+
+class PositionSelector extends PureComponent<PositionSelectorProps, PositionSelectorState> {
+  //
+  // static defaultProps: PositionSelectorProps = { debounceTimeOut: 800 };
+
+  constructor(props: PositionSelectorProps) {
+    super(props);
+    this.state = {
+      value: props.value,
+      options: [],
+    };
+    console.log(' ---------- 0s s  debounceTimeOut   ', props.debounceTimeOut);
+
+    this.onSearch = _.debounce(this.onSearch.bind(this), props.debounceTimeOut);
+
+    this.triggerChange = _.debounce(this.triggerChange.bind(this), 300);
+  }
+
+  componentDidMount(): void {
+    this.onSearch('');
+  }
+
+  onSearch = (value) => {
+    console.log(' --- --- ===== === value: ', value);
+    orgService.querySelect({ queryValue: value }).then((res) => {
+      const { burden } = res;
+      if (burden && burden.list) {
+        console.log(' --- --- ===== === burden: ', burden.list);
+
+        let nd = [];
+        burden.list.map((item) => {
+          return nd.push({
+            ...item,
+            label: item.name,
+            value: item.id,
+          });
+        });
+        this.setState({ options: nd });
+      }
+    });
+  };
+
+  static getDerivedStateFromProps(nextProps, state) {
+    if ('value' in nextProps) {
+      return {
+        ...state,
+        value: nextProps.value,
+      };
+    }
+    return state;
+  }
+
+  onChange = (value: string, option) => {
+    this.triggerChange(value, option);
+  };
+
+  onSelect = (value: string, option) => {
+    this.triggerChange(value, option);
+  };
+
+  triggerChange = (value, option) => {
+    const { onChange } = this.props;
+    if (onChange) {
+      onChange(value, option);
+    }
+  };
+
+  render() {
+    //
+    const { ...restProps } = this.props;
+    const { value, options, defaultValue } = this.state;
+
+    return (
+      <Select
+        {...restProps}
+        showSearch
+        // fieldNames={{ value: 'id', label: 'name', options: 'sname' }}
+        onSearch={(val) => {
+          this.onSearch(val);
+        }}
+        // defaultValue={defaultValue}
+        onChange={(val, option) => {
+          this.onChange(val, option);
+        }}
+        onSelect={(val, option) => {
+          this.onSelect(val, option);
+        }}
+        options={options}
+        value={value}
+      />
+    );
+  }
+}
+
+PositionSelector.defaultProps = defaultProps;
+
+export default PositionSelector;
