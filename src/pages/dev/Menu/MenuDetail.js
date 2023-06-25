9a8428a502d5cb673bde18279c3c96021a705836
hello <keye.myria@gmail.com>
2023-06-25 17:44:59 +0800

    menu detail

--- src/pages/dev/Menu/MenuDetail.js
+++ src/pages/dev/Menu/MenuDetail.js
@@ -64,40 +64,56 @@ class MenuDetail extends PureComponent {
           {/* <ProDescriptions.Item label="资产数量" key="asset_amount">
             {detailData.asset_amount}
           </ProDescriptions.Item> */}
+
+          <ProDescriptions.Item label="层级" key="level">
+            {detailData.level}
+          </ProDescriptions.Item>
+
           <ProDescriptions.Item label="是否末级" key="is_leaf">
             {detailData.is_leaf ? '是' : '否'}
           </ProDescriptions.Item>
-        </ProDescriptions>
 
-        <ProDescriptions column={3} title="动作">
-          {detailData.actions &&
-            detailData.actions.map((item) => (
-              <ProDescriptions.Item label={item.name} key={item.code}>
-                <Tag color="green">{item.code}</Tag>
-                <Tooltip title="">
-                  <QuestionCircleOutlined />
-                </Tooltip>
-              </ProDescriptions.Item>
-            ))}
-        </ProDescriptions>
+          <ProDescriptions.Item label="打开新标签" key="open_blank">
+            {detailData.open_blank === true ? (
+              <Badge status="success" text="是" />
+            ) : (
+              <Badge status="error" text="否" />
+            )}
+          </ProDescriptions.Item>
 
-        <Divider />
-        <ProDescriptions column={1} title="其它">
-          <ProDescriptions.Item label="状态">
+          <ProDescriptions.Item label="是否显示">
             {detailData.is_show ? (
               <Badge status="success" text="显示" />
             ) : (
               <Badge status="error" text="不显示" />
             )}
           </ProDescriptions.Item>
-          <ProDescriptions.Item label="状态">
-            {detailData.status === 1 ? (
+          <ProDescriptions.Item label="状态" key="is_active">
+            {detailData.is_active === true ? (
               <Badge status="success" text="启用" />
             ) : (
               <Badge status="error" text="停用" />
             )}
           </ProDescriptions.Item>
-          <ProDescriptions.Item label="备注">{detailData.memo}</ProDescriptions.Item>
+          <ProDescriptions.Item label="备注" key="memo">
+            {detailData.memo}
+          </ProDescriptions.Item>
+        </ProDescriptions>
+
+        <Divider />
+
+        <ProDescriptions column={4} title="动作">
+          {detailData.actions &&
+            detailData.actions.map((item) => (
+              <>
+                <ProDescriptions.Item span={1} label={item.name} key={item.code}>
+                  <Tag color="green">{item.code}</Tag>
+                </ProDescriptions.Item>
+                <ProDescriptions.Item span={3} label="资源" key="resources" valueType="jsonCode">
+                  {item.resources && JSON.stringify(item.resources)}
+                </ProDescriptions.Item>
+              </>
+            ))}
         </ProDescriptions>
       </Drawer>
     );
